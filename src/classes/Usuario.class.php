c590ded6d9b0328f20b82028468ab2ca621e0a80
DouglasRonchi <douglasronchi02@hotmail.com>
2019-08-15 11:03:21 -0300

    Adicionado as classes de usuario e veiculos

---
+++ src/classes/Usuario.class.php
@@ -0,0 +1,445 @@
+<?php
+
+class Usuario extends Site {
+
+    protected $id;
+    protected $usuario;
+    protected $senha;
+    protected $cadastro;
+    protected $nome;
+    protected $rg;
+    protected $cpf;
+    protected $endereco;
+    protected $estado;
+    protected $cidade;
+    protected $bairro;
+    protected $telefone;
+    protected $data_admissao;
+    protected $data_nascimento;
+    protected $cnh;
+    protected $vencimento_cnh;
+    protected $mopp;
+    protected $vencimento_mopp;
+    protected $data_cadastro;
+    protected $tipo_usuario;
+    protected $previlegio;
+    protected $online;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsuario()
+    {
+        return $this->usuario;
+    }
+
+    /**
+     * @param mixed $usuario
+     * @return Usuario
+     */
+    public function setUsuario($usuario)
+    {
+        $this->usuario = $usuario;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSenha()
+    {
+        return $this->senha;
+    }
+
+    /**
+     * @param mixed $senha
+     * @return Usuario
+     */
+    public function setSenha($senha)
+    {
+        $this->senha = $senha;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCadastro()
+    {
+        return $this->cadastro;
+    }
+
+    /**
+     * @param mixed $cadastro
+     * @return Usuario
+     */
+    public function setCadastro($cadastro)
+    {
+        $this->cadastro = $cadastro;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param mixed $nome
+     * @return Usuario
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRg()
+    {
+        return $this->rg;
+    }
+
+    /**
+     * @param mixed $rg
+     * @return Usuario
+     */
+    public function setRg($rg)
+    {
+        $this->rg = $rg;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCpf()
+    {
+        return $this->cpf;
+    }
+
+    /**
+     * @param mixed $cpf
+     * @return Usuario
+     */
+    public function setCpf($cpf)
+    {
+        $this->cpf = $cpf;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEndereco()
+    {
+        return $this->endereco;
+    }
+
+    /**
+     * @param mixed $endereco
+     * @return Usuario
+     */
+    public function setEndereco($endereco)
+    {
+        $this->endereco = $endereco;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    /**
+     * @param mixed $estado
+     * @return Usuario
+     */
+    public function setEstado($estado)
+    {
+        $this->estado = $estado;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCidade()
+    {
+        return $this->cidade;
+    }
+
+    /**
+     * @param mixed $cidade
+     * @return Usuario
+     */
+    public function setCidade($cidade)
+    {
+        $this->cidade = $cidade;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBairro()
+    {
+        return $this->bairro;
+    }
+
+    /**
+     * @param mixed $bairro
+     * @return Usuario
+     */
+    public function setBairro($bairro)
+    {
+        $this->bairro = $bairro;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTelefone()
+    {
+        return $this->telefone;
+    }
+
+    /**
+     * @param mixed $telefone
+     * @return Usuario
+     */
+    public function setTelefone($telefone)
+    {
+        $this->telefone = $telefone;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDataAdmissao()
+    {
+        return $this->data_admissao;
+    }
+
+    /**
+     * @param mixed $data_admissao
+     * @return Usuario
+     */
+    public function setDataAdmissao($data_admissao)
+    {
+        $this->data_admissao = $data_admissao;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDataNascimento()
+    {
+        return $this->data_nascimento;
+    }
+
+    /**
+     * @param mixed $data_nascimento
+     * @return Usuario
+     */
+    public function setDataNascimento($data_nascimento)
+    {
+        $this->data_nascimento = $data_nascimento;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCnh()
+    {
+        return $this->cnh;
+    }
+
+    /**
+     * @param mixed $cnh
+     * @return Usuario
+     */
+    public function setCnh($cnh)
+    {
+        $this->cnh = $cnh;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVencimentoCnh()
+    {
+        return $this->vencimento_cnh;
+    }
+
+    /**
+     * @param mixed $vencimento_cnh
+     * @return Usuario
+     */
+    public function setVencimentoCnh($vencimento_cnh)
+    {
+        $this->vencimento_cnh = $vencimento_cnh;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMopp()
+    {
+        return $this->mopp;
+    }
+
+    /**
+     * @param mixed $mopp
+     * @return Usuario
+     */
+    public function setMopp($mopp)
+    {
+        $this->mopp = $mopp;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVencimentoMopp()
+    {
+        return $this->vencimento_mopp;
+    }
+
+    /**
+     * @param mixed $vencimento_mopp
+     * @return Usuario
+     */
+    public function setVencimentoMopp($vencimento_mopp)
+    {
+        $this->vencimento_mopp = $vencimento_mopp;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDataCadastro()
+    {
+        return $this->data_cadastro;
+    }
+
+    /**
+     * @param mixed $data_cadastro
+     * @return Usuario
+     */
+    public function setDataCadastro($data_cadastro)
+    {
+        $this->data_cadastro = $data_cadastro;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTipoUsuario()
+    {
+        return $this->tipo_usuario;
+    }
+
+    /**
+     * @param mixed $tipo_usuario
+     * @return Usuario
+     */
+    public function setTipoUsuario($tipo_usuario)
+    {
+        $this->tipo_usuario = $tipo_usuario;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrevilegio()
+    {
+        return $this->previlegio;
+    }
+
+    /**
+     * @param mixed $previlegio
+     * @return Usuario
+     */
+    public function setPrevilegio($previlegio)
+    {
+        $this->previlegio = $previlegio;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOnline()
+    {
+        return $this->online;
+    }
+
+    /**
+     * @param mixed $online
+     * @return Usuario
+     */
+    public function setOnline($online)
+    {
+        $this->online = $online;
+        return $this;
+    }
+
+
+    /**
+     * Função de cadastro de usuários
+     * @return true ou false
+     */
+    public function cadastrarUsuario(){
+
+
+    }
+
+    /**
+     * Função de atualizar de usuários
+     * @return true ou false
+     */
+    public function atualizarUsuario(){
+
+
+    }
+
+    /**
+     * Função para deletar usuários
+     * @return true ou false
+     */
+    public function deletarUsuario(){
+
+
+    }
+
+
+
+
+}
---
+++ src/classes/Veiculo.class.php
@@ -0,0 +1,238 @@
+<?php
+
+class Veiculo extends Site {
+
+    protected $id;
+    protected $frota;
+    protected $marca;
+    protected $modelo;
+    protected $placa;
+    protected $chassi;
+    protected $renavam;
+    protected $capacidade_tanque;
+    protected $ano_fabricacao;
+    protected $ano_modelo;
+    protected $capacidade_carga;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFrota()
+    {
+        return $this->frota;
+    }
+
+    /**
+     * @param mixed $frota
+     * @return Veiculo
+     */
+    public function setFrota($frota)
+    {
+        $this->frota = $frota;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMarca()
+    {
+        return $this->marca;
+    }
+
+    /**
+     * @param mixed $marca
+     * @return Veiculo
+     */
+    public function setMarca($marca)
+    {
+        $this->marca = $marca;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getModelo()
+    {
+        return $this->modelo;
+    }
+
+    /**
+     * @param mixed $modelo
+     * @return Veiculo
+     */
+    public function setModelo($modelo)
+    {
+        $this->modelo = $modelo;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPlaca()
+    {
+        return $this->placa;
+    }
+
+    /**
+     * @param mixed $placa
+     * @return Veiculo
+     */
+    public function setPlaca($placa)
+    {
+        $this->placa = $placa;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getChassi()
+    {
+        return $this->chassi;
+    }
+
+    /**
+     * @param mixed $chassi
+     * @return Veiculo
+     */
+    public function setChassi($chassi)
+    {
+        $this->chassi = $chassi;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRenavam()
+    {
+        return $this->renavam;
+    }
+
+    /**
+     * @param mixed $renavam
+     * @return Veiculo
+     */
+    public function setRenavam($renavam)
+    {
+        $this->renavam = $renavam;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCapacidadeTanque()
+    {
+        return $this->capacidade_tanque;
+    }
+
+    /**
+     * @param mixed $capacidade_tanque
+     * @return Veiculo
+     */
+    public function setCapacidadeTanque($capacidade_tanque)
+    {
+        $this->capacidade_tanque = $capacidade_tanque;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAnoFabricacao()
+    {
+        return $this->ano_fabricacao;
+    }
+
+    /**
+     * @param mixed $ano_fabricacao
+     * @return Veiculo
+     */
+    public function setAnoFabricacao($ano_fabricacao)
+    {
+        $this->ano_fabricacao = $ano_fabricacao;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAnoModelo()
+    {
+        return $this->ano_modelo;
+    }
+
+    /**
+     * @param mixed $ano_modelo
+     * @return Veiculo
+     */
+    public function setAnoModelo($ano_modelo)
+    {
+        $this->ano_modelo = $ano_modelo;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCapacidadeCarga()
+    {
+        return $this->capacidade_carga;
+    }
+
+    /**
+     * @param mixed $capacidade_carga
+     * @return Veiculo
+     */
+    public function setCapacidadeCarga($capacidade_carga)
+    {
+        $this->capacidade_carga = $capacidade_carga;
+        return $this;
+    }
+
+
+
+
+
+    /**
+     * Função de cadastro de veículos
+     * @return true ou false
+     */
+    public function cadastrarVeiculo(){
+
+
+    }
+
+    /**
+     * Função de atualizar de veículos
+     * @return true ou false
+     */
+    public function atualizarVeiculo(){
+
+
+    }
+
+    /**
+     * Função para deletar veículos
+     * @return true ou false
+     */
+    public function deletarVeiculo(){
+
+
+    }
+
+
+
+}
