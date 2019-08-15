c15883e2a0b01b83355be1503d69fdfbc84a623e
dauanaana <dauanasvro@gmail.com>
2019-08-15 11:35:29 -0300

    relatorio de veiculos modificado

--- src/pages/paginagestor/relatorio_veiculos.php
+++ src/pages/paginagestor/relatorio_veiculos.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,15 +11,18 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Pojigo - Início</title>
+  <title>Pojigo - Relatório de Veiculos</title>
 
   <!-- Custom fonts for this template-->
   <link href="../../../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+  rel="stylesheet">
 
   <!-- Custom styles for this template-->
   <link href="../../../css/sb-admin-2.min.css" rel="stylesheet">
 
+  <!-- Custom styles for this page -->
+  <link href="../../../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 </head>
 
 <body id="page-top">
@@ -34,7 +39,7 @@
 
       <!-- Main Content -->
       <div id="content">
-        
+
         <!-- Topbar Navbar -->
         <?php include '../menu/topbar.php'; ?>
         <!-- End of Topbar -->
@@ -45,14 +50,66 @@
           <!-- Page Heading -->
           <h1 class="h3 mb-4 text-gray-800">Relatório de Veículos</h1>
 
+
+          <!-- DataTales Example -->
+          <div class="card shadow mb-4">
+            <div class="card-header py-3">
+              <h6 class="m-0 font-weight-bold text-primary">Tabela de Dados</h6>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                  <thead>
+                    <tr>
+                      <th>Cadastro</th>
+                      <th>Marca Veículo</th>
+                      <th>Modelo Veículo</th>
+                      <th>Placa</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tfoot>
+                    
+                  </tfoot>
+                  <tbody>
+                   <tr>
+                    <td>1234</td>
+                    <td>BMW</td>
+                    <td>conversivel</td>
+                    <td>ABC1233</td>
+                    <td><center><button type="button" class="btn btn-outline-primary">Visualizar</button></center></td>
+                  </tr>
+                    <tr>
+                    <td>1478</td>
+                    <td>Chevrolet</td>
+                    <td>WK</td>
+                    <td>QWE789</td>
+                    <td><center><button type="button" class="btn btn-outline-primary">Visualizar</button></center></td>
+                  </tr>
+                    <tr>
+                    <td>9854</td>
+                    <td>Ford</td>
+                    <td>Klo</td>
+                    <td>SDR852</td>
+                    <td><center><button type="button" class="btn btn-outline-primary">Visualizar</button></center></td>
+                  </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+
+          
+
+
         </div>
-        <!-- /.container-fluid -->
+        <!-- /..container-fluid -->
 
       </div>
       <!-- End of Main Content -->
 
       <!-- Footer -->
-      <?php include '../menu/footer.php'; ?>      
+      <?php include '../menu/footer.php'; ?>
       <!-- End of Footer -->
 
     </div>
@@ -79,6 +136,12 @@
   <!-- Custom scripts for all pages-->
   <script src="../../../js/sb-admin-2.min.js"></script>
 
+  <!-- Page level plugins -->
+  <script src="../../../vendor/datatables/jquery.dataTables.min.js"></script>
+  <script src="../../../vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="../../../js/demo/datatables-demo.js"></script>
 </body>
 
 </html>
