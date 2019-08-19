6cef88a6fea906e05fb52553578a8d9d543335cf
DouglasRonchi <douglasronchi02@hotmail.com>
2019-08-19 09:43:33 -0300

    Ajustando Registros

--- src/pages/paginagestor/registros.php
+++ src/pages/paginagestor/registros.php
@@ -3,26 +3,30 @@
 
 <head>
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <title>Pojigo - Início</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Pojigo - Início</title>
 
-  <!-- Custom fonts for this template-->
-  <link href="../../../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <!-- Custom fonts for this template-->
+    <link href="../../../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+          rel="stylesheet">
 
-  <!-- Custom styles for this template-->
-  <link href="../../../css/sb-admin-2.min.css" rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="../../../css/sb-admin-2.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this page -->
+    <link href="../../../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
 </head>
 
 <body id="page-top">
 
-  <!-- Page Wrapper -->
-  <div id="wrapper">
+<!-- Page Wrapper -->
+<div id="wrapper">
 
     <!-- Sidebar -->
     <?php include '../menu/sidebar.php'; ?>
@@ -31,163 +35,193 @@
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
 
-      <!-- Main Content -->
-      <div id="content">
+        <!-- Main Content -->
+        <div id="content">
 
-        <!-- Topbar Navbar -->
-        <?php include '../menu/topbar.php'; ?>
-        <!-- End of Topbar -->
+            <!-- Topbar Navbar -->
+            <?php include '../menu/topbar.php'; ?>
+            <!-- End of Topbar -->
 
-        <!-- Begin Page Content -->
-        <div class="container-fluid">
+            <!-- Begin Page Content -->
+            <div class="container-fluid">
 
-          <!-- Page Heading -->
-          <h1 class="h3 mb-4 text-gray-800"><center>REGISTROS</center></h1>
-          <h1 class="h3 mb-2 text-gray-800"></h1>
-          <p class="mb-4"><a target="_blank" href="https://datatables.net"></a></p>
+                <!-- Page Heading -->
+                <h1 class="h3 mb-4 text-gray-800">
+                    <center>REGISTROS</center>
+                </h1>
+                <h1 class="h3 mb-2 text-gray-800"></h1>
+                <p class="mb-4"><a target="_blank" href="https://datatables.net"></a></p>
 
-          <!-- DataTales Example -->
-          <div class="card shadow mb-4">
-            <div class="card-header py-3">
-              
-          <form class="form-inline " style="padding-left:25rem!important">
-            <h5>Nome</h5>
-            <div class="form-group mx-sm-3 mb-2">
-              <input type="text" class="form-control" id="inputPassword2" placeholder="">
-              <button type="button" class="btn btn-outline-primary ml-3">Procurar</button>
+                <!-- DataTales Example -->
+                <div class="card shadow mb-4">
+                    <div class="card-header py-3">
+
+                        <form class="form-inline " style="padding-left:25rem!important">
+                            <h5>Nome</h5>
+                            <div class="form-group mx-sm-3 mb-2">
+                                <input type="text" class="form-control" id="inputPassword2" placeholder="">
+                                <button type="button" class="btn btn-outline-primary ml-3">Procurar</button>
+                            </div>
+                        </form>
+                        <h6 class="m-0 font-weight-bold text-primary"></h6>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                <thead>
+                                <tr>
+                                    <th>Inicio de Jornada</th>
+                                    <th>Inicio de Intervalo</th>
+                                    <th>Fim de Intervalo</th>
+                                    <th>Inicio de Intervalo</th>
+                                    <th>Fim de Intervalo</th>
+                                    <th>Inicio de Intervalo</th>
+                                    <th>Fim de Intervalo</th>
+                                    <th>Fim de Jornada</th>
+                                    <th></th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>219060</td>
+                                    <td>209746</td>
+                                    <td>123696</td>
+                                    <td>074618</td>
+                                    <td>018641</td>
+                                    <td>219060</td>
+                                    <td>209746</td>
+                                    <td>123666</td>
+                                    <td>
+                                        <center>
+                                            <button type="button" class="btn btn-outline-primary">Visualizar</button>
+                                        </center>
+                                    </td>
+
+                                </tr>
+                                <tr>
+                                    <td>219060</td>
+                                    <td>962455</td>
+                                    <td>123696</td>
+                                    <td>074618</td>
+                                    <td>018646</td>
+                                    <td>219060</td>
+                                    <td>962455</td>
+                                    <td>123666</td>
+                                    <td>
+                                        <center>
+                                            <button type="button" class="btn btn-outline-primary">Visualizar</button>
+                                        </center>
+                                    </td>
+
+                                </tr>
+                                <tr>
+                                    <td>219060</td>
+                                    <td>145675</td>
+                                    <td>123696</td>
+                                    <td>074618</td>
+                                    <td>018646</td>
+                                    <td>219060</td>
+                                    <td>145675</td>
+                                    <td>123666</td>
+                                    <td>
+                                        <center>
+                                            <button type="button" class="btn btn-outline-primary">Visualizar</button>
+                                        </center>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>219060</td>
+                                    <td>729652</td>
+                                    <td>123696</td>
+                                    <td>074618</td>
+                                    <td>018646</td>
+                                    <td>219060</td>
+                                    <td>729652</td>
+                                    <td>123666</td>
+                                    <td>
+                                        <center>
+                                            <button type="button" class="btn btn-outline-primary">Visualizar</button>
+                                        </center>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>219060</td>
+                                    <td>462557</td>
+                                    <td>123696</td>
+                                    <td>074618</td>
+                                    <td>018646</td>
+                                    <td>219060</td>
+                                    <td>462557</td>
+                                    <td>123666</td>
+                                    <td>
+                                        <center>
+                                            <button type="button" class="btn btn-outline-primary">Visualizar</button>
+                                        </center>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>219060</td>
+                                    <td>265555</td>
+                                    <td>123696</td>
+                                    <td>074618</td>
+                                    <td>018648</td>
+                                    <td>219060</td>
+                                    <td>265555</td>
+                                    <td>123666</td>
+                                    <td>
+                                        <center>
+                                            <button type="button" class="btn btn-outline-primary">Visualizar</button>
+                                        </center>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>219060</td>
+                                    <td>434675</td>
+                                    <td>123696</td>
+                                    <td>074618</td>
+                                    <td>018645</td>
+                                    <td>219060</td>
+                                    <td>434675</td>
+                                    <td>123666</td>
+                                    <td>
+                                        <center>
+                                            <button type="button" class="btn btn-outline-primary">Visualizar</button>
+                                        </center>
+                                    </td>
+                                </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-          </form>
-              <h6 class="m-0 font-weight-bold text-primary"></h6>
-            </div>
-            <div class="card-body">
-              <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                  <thead>
-                    <tr>
-                      <th>Inicio de Jornada</th>
-                      <th>Inicio de Intervalo</th>
-                      <th>Fim de Intervalo</th>
-                      <th>Inicio de Intervalo </th>
-                      <th>Fim de Intervalo</th>
-                      <th>Inicio de Intervalo </th>
-                      <th>Fim de Intervalo</th>
-                      <th>Fim de Jornada</th>
-                      <th></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>219060</td>
-                      <td>209746</td>
-                      <td>123696</td>
-                      <td>074618</td>
-                      <td>018641</td>
-                      <td>219060</td>
-                      <td>209746</td>
-                      <td>123666</td>
-                      <td><center><button type="button" class="btn btn-outline-primary">Visualizar</button></center></td>
-                      
-                    </tr>
-                    <tr>
-                      <td>219060</td>
-                      <td>962455</td>
-                      <td>123696</td>
-                      <td>074618</td>
-                      <td>018646</td>
-                      <td>219060</td>
-                      <td>962455</td>
-                      <td>123666</td>
-                      <td><center><button type="button" class="btn btn-outline-primary">Visualizar</button></center></td>
-                      
-                    </tr>
-                    <tr>
-                      <td>219060</td>
-                      <td>145675</td>
-                      <td>123696</td>
-                      <td>074618</td>
-                      <td>018646</td>
-                      <td>219060</td>
-                      <td>145675</td>
-                      <td>123666</td>
-                      <td><center><button type="button" class="btn btn-outline-primary">Visualizar</button></center></td>
-                    </tr>
-                    <tr>
-                      <td>219060</td>
-                      <td>729652</td>
-                      <td>123696</td>
-                      <td>074618</td>
-                      <td>018646</td>
-                      <td>219060</td>
-                      <td>729652</td>
-                      <td>123666</td>
-                      <td><center><button type="button" class="btn btn-outline-primary">Visualizar</button></center></td>
-                    </tr>
-                    <tr>
-                      <td>219060</td>
-                      <td>462557</td>
-                      <td>123696</td>
-                      <td>074618</td>
-                      <td>018646</td>
-                      <td>219060</td>
-                      <td>462557</td>
-                      <td>123666</td>
-                      <td><center><button type="button" class="btn btn-outline-primary">Visualizar</button></center></td>
-                    </tr>
-                    <tr>
-                      <td>219060</td>
-                      <td>265555</td>
-                      <td>123696</td>
-                      <td>074618</td>
-                      <td>018648</td>
-                      <td>219060</td>
-                      <td>265555</td>
-                      <td>123666</td>
-                      <td><center><button type="button" class="btn btn-outline-primary">Visualizar</button></center></td>
-                    </tr>
-                    <tr>
-                      <td>219060</td>
-                      <td>434675</td>
-                      <td>123696</td>
-                      <td>074618</td>
-                      <td>018645</td>
-                      <td>219060</td>
-                      <td>434675</td>
-                      <td>123666</td>
-                      <td><center><button type="button" class="btn btn-outline-primary">Visualizar</button></center></td>
-                    </tr>
-
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
+            <!-- /.container-fluid -->
 
         </div>
-        <!-- /.container-fluid -->
+        <!-- End of Main Content -->
 
-      </div>
-      <!-- End of Main Content -->
-
-      <!-- Footer -->
-      <footer class="sticky-footer bg-white">
-        <div class="container my-auto">
-          <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Your Website 2019</span>
-          </div>
-        </div>
-      </footer>
-      <!-- End of Footer -->
+        <!-- Footer -->
+        <footer class="sticky-footer bg-white">
+            <div class="container my-auto">
+                <div class="copyright text-center my-auto">
+                    <span>Copyright &copy; Your Website 2019</span>
+                </div>
+            </div>
+        </footer>
+        <!-- End of Footer -->
 
     </div>
     <!-- End of Content Wrapper -->
 
-  </div>
-  <!-- End of Page Wrapper -->
+</div>
+<!-- End of Page Wrapper -->
 
-  <!-- Scroll to Top Button-->
-  <a class="scroll-to-top rounded" href="#page-top">
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
-  </a>
+</a>
 
 
 </div>
@@ -195,7 +229,6 @@
 
 </div>
 <!-- End of Main Content -->
-
 
 
 </div>
@@ -206,7 +239,7 @@
 
 <!-- Scroll to Top Button-->
 <a class="scroll-to-top rounded" href="#page-top">
-  <i class="fas fa-angle-up"></i>
+    <i class="fas fa-angle-up"></i>
 </a>
 
 <!-- Logout Modal-->
@@ -228,8 +261,7 @@
 <!-- Page level custom scripts -->
 <script src="../../../js/demo/datatables-demo.js"></script>
 
-  <?php include_once '../../include/configdatatable.php'?>
-
+<?php include_once '../../include/configdatatable.php' ?>
 
 
 </body>
