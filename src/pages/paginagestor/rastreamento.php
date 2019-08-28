c690abdaf2a2f411a113bd2b07f3512bb2d6c0fa
DouglasRonchi <douglasronchi02@hotmail.com>
2019-08-28 09:56:23 -0300

    Configurando Coordenadas

--- src/pages/paginagestor/rastreamento.php
+++ src/pages/paginagestor/rastreamento.php
@@ -9,27 +9,31 @@ $conn = new Site;
 
 <head>
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-  <title>Pojigo - Início</title>
+    <title>Pojigo - Início</title>
 
-  <!-- Custom fonts for this template-->
-  <link href="../../../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <!-- Custom fonts for this template-->
+    <link href="../../../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+          rel="stylesheet">
 
-  <!-- Custom styles for this template-->
-  <link href="../../../css/sb-admin-2.min.css" rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="../../../css/sb-admin-2.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this page -->
+    <link href="../../../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
 </head>
 
 <body id="page-top">
 
-  <!-- Page Wrapper -->
-  <div id="wrapper">
+<!-- Page Wrapper -->
+<div id="wrapper">
 
     <!-- Sidebar -->
     <?php include '../menu/sidebar.php'; ?>
@@ -38,52 +42,122 @@ $conn = new Site;
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
 
-      <!-- Main Content -->
-      <div id="content">
-        
-        <!-- Topbar Navbar -->
-        <?php include '../menu/topbar.php'; ?>
-        <!-- End of Topbar -->
+        <!-- Main Content -->
+        <div id="content">
 
-        <!-- Begin Page Content -->
-        <div class="container-fluid">
+            <!-- Topbar Navbar -->
+            <?php include '../menu/topbar.php'; ?>
+            <!-- End of Topbar -->
 
-          <!-- Page Heading -->
-          <h1 class="h3 mb-4 text-gray-800">Rastreamento</h1>
+            <!-- Begin Page Content -->
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <h1 class="h3 mb-4 text-gray-800">Rastreamentos</h1>
+
+                <div style="height: 400px; background: green">A</div>
+
+
+                <!-- DataTales Rastreamento -->
+                <div class="card shadow mb-4">
+                    <div class="card-header py-3">
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                <thead>
+                                <tr>
+                                    <th>Frota</th>
+                                    <th>Placa</th>
+                                    <th>Última Posição</th>
+                                    <th>Motorista</th>
+                                    <th>Info</th>
+                                    <th>Visualizar</th>
+                                </tr>
+                                </thead>
+                                <tfoot>
+                                <tr>
+                                    <th>Frota</th>
+                                    <th>Placa</th>
+                                    <th>Última Posição</th>
+                                    <th>Motorista</th>
+                                    <th>Info</th>
+                                    <th>Visualizar</th>
+                                </tr>
+                                </tfoot>
+                                <tbody>
+                                <?php
+                                $selectUsers = $conn->executeQuery("SELECT * FROM usuario");
+                                $selectUsersRows = mysqli_num_rows($selectUsers);
+                                while ($selectUsersRows = mysqli_fetch_assoc($selectUsers)):
+                                    ?>
+                                    <tr>
+                                        <td><?= $selectUsersRows["cadastro"] ?></td>
+                                        <td><?= $selectUsersRows["nome"] ?></td>
+                                        <td><?= $selectUsersRows["cpf"] ?></td>
+                                        <td><?= $selectUsersRows["rg"] ?></td>
+                                        <td><?= $selectUsersRows["tipo_usuario"] ?></td>
+                                        <td>
+                                            <form action="" method="post">
+                                                <div class="btn-group btn-block">
+                                                    <button class="btn btn-sm btn-primary" name="btnEditar">Editar</button>
+                                                </div>
+
+                                            </form>
+                                        </td>
+                                    </tr>
+                                <?php endwhile; ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+            </div>
+            <!-- /.container-fluid -->
 
         </div>
-        <!-- /.container-fluid -->
+        <!-- End of Main Content -->
 
-      </div>
-      <!-- End of Main Content -->
-
-      <!-- Footer -->
-      <?php include '../menu/footer.php'; ?>      
-      <!-- End of Footer -->
+        <!-- Footer -->
+        <?php include '../menu/footer.php'; ?>
+        <!-- End of Footer -->
 
     </div>
     <!-- End of Content Wrapper -->
 
-  </div>
-  <!-- End of Page Wrapper -->
+</div>
+<!-- End of Page Wrapper -->
 
-  <!-- Scroll to Top Button-->
-  <a class="scroll-to-top rounded" href="#page-top">
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
-  </a>
+</a>
 
-  <!-- Logout Modal-->
-  <?php include '../menu/logoutmodal.php'; ?>
+<!-- Logout Modal-->
+<?php include '../menu/logoutmodal.php'; ?>
 
-  <!-- Bootstrap core JavaScript-->
-  <script src="../../../vendor/jquery/jquery.min.js"></script>
-  <script src="../../../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- Bootstrap core JavaScript-->
+<script src="../../../vendor/jquery/jquery.min.js"></script>
+<script src="../../../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-  <!-- Core plugin JavaScript-->
-  <script src="../../../vendor/jquery-easing/jquery.easing.min.js"></script>
+<!-- Core plugin JavaScript-->
+<script src="../../../vendor/jquery-easing/jquery.easing.min.js"></script>
 
-  <!-- Custom scripts for all pages-->
-  <script src="../../../js/sb-admin-2.min.js"></script>
+<!-- Custom scripts for all pages-->
+<script src="../../../js/sb-admin-2.min.js"></script>
+
+<!-- Page level plugins -->
+<script src="../../../vendor/datatables/jquery.dataTables.min.js"></script>
+<script src="../../../vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+<!-- Page level custom scripts -->
+<script src="../../../js/demo/datatables-demo.js"></script>
+
+<?php include_once '../../include/configdatatable.php'?>
 
 </body>
 
