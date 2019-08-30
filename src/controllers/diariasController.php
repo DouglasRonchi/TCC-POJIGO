760b93949c5b79b30019b2ecc070d258618fb335
Jonas4955 <jonas.antunes4955@gmail.com>
2019-08-30 10:53:31 -0300

    Alteração nos arquivos diariasController e diarias.php

--- src/controllers/diariasController.php
+++ src/controllers/diariasController.php
@@ -2,3 +2,15 @@
 require_once '../classes/Autoload.class.php';
 $conn = new Site;
 
+$sqlDiaria = $conn->executeQuery("SELECT * FROM usuario WHERE previlegio = 3");
+$resultado = mysqli_fetch_array($sqlDiaria);
+$id = $resultado['usuario_id'];
+
+
+
+$sqlDiaria = $conn->executeQuery('SELECT SUM(dia.valor), rp.fk_usuario FROM registro_ponto rp JOIN diarias dia ON dia.id = rp.fk_diaria WHERE rp.fk_usuario = '.$id.'');
+$diaria = mysqli_fetch_assoc($sqlDiaria);
+
+echo "<pre>";
+var_dump($diaria);
+echo "</pre>";
