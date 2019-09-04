e721f28358a522d36d2bbce8e96f9e1c29ede7a0
DouglasRonchi <douglasronchi02@hotmail.com>
2019-09-04 11:12:33 -0300

    Configurando

--- src/pages/menu/sidebar.php
+++ src/pages/menu/sidebar.php
@@ -2,7 +2,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashboard.php">
       <div class="sidebar-brand-icon">
         <img src="../../../img/fav.png" alt="logotigo POJIGO">
       </div>
