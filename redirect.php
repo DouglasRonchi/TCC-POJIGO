d6461501ed62192b600e324d636547e37d9018ef
DouglasRonchi <douglasronchi02@hotmail.com>
2019-09-05 11:53:48 -0300

    Ajustando dashboard

--- redirect.php
+++ redirect.php
@@ -3,9 +3,9 @@ require_once 'src/classes/Autoload.class.php';
 session_start();
 
 if ($_SESSION['previlegio']=='1'){
-    header('Location: src/pages/paginagestor/index.php');
+    header('Location: src/pages/paginagestor/dashboard.php');
 } else if ($_SESSION['previlegio']=='2'){
-    header('Location: src/pages/paginagestor/index.php');
+    header('Location: src/pages/paginagestor/dashboard.php');
 } else if ($_SESSION['previlegio']=='3'){
     header('Location: src/pages/paginamotorista/index.php');
 } else {
