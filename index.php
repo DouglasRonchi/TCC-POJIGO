9fd4c857656507859e74a6555be1e9c48b09b864
DouglasRonchi <douglasronchi02@hotmail.com>
2019-09-05 09:44:49 -0300

    Consertanto Login e Senha

--- index.php
+++ index.php
@@ -2,84 +2,85 @@
 <html lang="en" dir="ltr">
 <head>
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-  <title>Pojigo - Login</title>
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+          rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="css/styleLogin.css" rel="stylesheet">
+    <!-- Favicon icon site -->
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
 
-  <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-  <!-- Custom styles for this template-->
-  <link href="css/sb-admin-2.min.css" rel="stylesheet">
-  <link href="css/styleLogin.css" rel="stylesheet">
-  <!-- Favicon icon site -->
-  <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"/>
+
+    <title>Pojigo - Login</title>
 
 </head>
 <body>
 
-  <form action="src/controllers/loginController.php" class="login-form" method="post">
+<form action="src/controllers/loginController.php" class="login-form" method="post">
     <div class="text-center logologin">
-      <h1>POJIGO</h1>
-      <small>Rotas & Registros</small>  
+        <h1>POJIGO</h1>
+        <small>Rotas & Registros</small>
     </div>
     <?php if (isset($_COOKIE['inv'])): ?>
-      <div class="alert alert-danger alert-dismissible animated--grow-in fade show" role="alert">
-          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-          </button>
-          <strong>Usuário ou senha Inválidos.</strong>
-      </div>
-      <?php endif; ?>
+        <div class="alert alert-danger alert-dismissible animated--grow-in fade show" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <strong>Usuário ou senha Inválidos.</strong>
+        </div>
+    <?php endif; ?>
 
-      <div class="txtb">
-      <input type="text" id="inputUser" name="inputUser">
-      <span data-placeholder="Usuário"></span>
+    <div class="txtb">
+        <input type="text" id="inputUser" name="inputUser">
+        <span data-placeholder="Usuário"></span>
     </div>
 
 
     <div class="txtb">
-      <input type="password" id="inputSenha" name="inputSenha">
-      <span data-placeholder="Senha"></span>
+        <input type="password" id="inputSenha" name="inputSenha">
+        <span data-placeholder="Senha"></span>
     </div>
 
     <input type="submit" class="logbtn btn btn-primary" name="btnLogar" value="Entrar">
 
     <div class="bottom-text">
-      <a href="forgotpassword.php">Esqueçeu a senha?</a>
+        <a href="forgotpassword.php">Esqueçeu a senha?</a>
     </div>
 
-  </form>
+</form>
 
 
+<!-- Logout Modal-->
 
-  <!-- Logout Modal-->
+<!-- Bootstrap core JavaScript-->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-  <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- Core plugin JavaScript-->
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
-  <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-  <!-- Custom scripts for all pages-->
-  <script src="js/sb-admin-2.min.js"></script>
+<!-- Custom scripts for all pages-->
+<script src="js/sb-admin-2.min.js"></script>
 
 
-  <script type="text/javascript">
-    $(".txtb input").on("focus",function(){
-      $(this).addClass("focus");
+<script type="text/javascript">
+    $(".txtb input").on("focus", function () {
+        $(this).addClass("focus");
     });
 
-    $(".txtb input").on("blur",function(){
-      if($(this).val() == "")
-        $(this).removeClass("focus");
+    $(".txtb input").on("blur", function () {
+        if ($(this).val() == "")
+            $(this).removeClass("focus");
     });
 
-  </script>
+</script>
 </body>
 </html>
