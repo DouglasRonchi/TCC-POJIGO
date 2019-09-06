8dce600dc70826a30bacb07d0c2c3213c10fce1d
Jonas4955 <jonas.antunes4955@gmail.com>
2019-09-06 02:08:37 -0300

    alterações em institucional

--- institucional.php
+++ institucional.php
@@ -7,15 +7,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="">
 <meta name="author" content="">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 <title>Pojigo - Início</title>
 
-<!-- Custom fonts for this template-->
-<!-- <link href="../../../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css"> -->
-<!-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"> -->
-<!-- Custom styles for this template-->
-<!-- <link href="../../../css/sb-admin-2.min.css" rel="stylesheet"> -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 <head>
   <style type="text/css">
@@ -60,9 +55,9 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav">
-            <a class="nav-item nav-link active text-light" href="#">Home<span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link active text-white" href="#">Home<span class="sr-only">(current)</span></a>
             <a class="nav-item nav-link text-light" href="contato.php">Fale conosco</a>
-            <a class="btn btn-primary  mt-2 text-white" href="index.php" role="button" style="margin-left: 1050px;"><strong>ENTRAR</strong></a>
+            <a class="btn nav-link text-white float-right" href="quemsomos.php"><strong>Quem somos</strong></a>
           </div>
         </div>
       </nav>
