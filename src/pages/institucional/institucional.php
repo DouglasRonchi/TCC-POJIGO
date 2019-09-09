424a88600ee06f94f8dd51cd70d70013046be52f
DouglasRonchi <douglasronchi02@hotmail.com>
2019-09-09 09:06:17 -0300

    Ajustando institucional

---
+++ src/pages/institucional/institucional.php
@@ -0,0 +1,314 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+    <!-- Custom fonts for this template-->
+    <link href="../../../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+          rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="../../../css/sb-admin-2.min.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="../../../css/institucional.css">
+
+<title>Pojigo - Início</title>
+
+</head>
+
+<body id="page-top" class="sidebar-toggled">
+
+  <div id="list-example" class="list-group">
+
+    <!-- Begin Page Content -->
+    <nav class="navbar navbar-expand-lg navbar-default">
+      <a class="navbar-brand text-primary"><img class="mr-2" src="../../../img/institucional/inicial_icons/navicon.png" height="30" width="30"><strong>Pojigo</strong></a>
+      <button class="navbar-toggler togle" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+
+        <svg class="ham hamRotate ham4" viewBox="0 0 100 100" width="60" onclick="this.classList.toggle('active')">
+          <path
+          class="line top"
+          d="m 70,33 h -40 c 0,0 -8.5,-0.149796 -8.5,8.5 0,8.649796 8.5,8.5 8.5,8.5 h 20 v -20" />
+          <path
+          class="line middle"
+          d="m 70,50 h -40" />
+          <path
+          class="line bottom"
+          d="m 30,67 h 40 c 0,0 8.5,0.149796 8.5,-8.5 0,-8.649796 -8.5,-8.5 -8.5,-8.5 h -20 v 20" />
+        </svg>
+
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link text-white menulink" href="#"><strong>Home</strong><span class="sr-only">(current)</span></a>
+          <a href="#list-item-4" class="nav-item nav-link text-white menulink"><strong>Quem somos</strong></a>
+          <a href="#list-item-5" class="nav-item nav-link text-white menulink"><strong>Fale conosco</strong></a>
+        </div>
+      </div>
+    </nav>
+
+    <div id="carouselExampleCaptions" class="carousel slide sombra" data-ride="carousel" data-wrap="true">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+      </ol>
+
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="../../../img/institucional/monitoramento.jpg" class="d-block w-100 imgcarousel" alt="..." height="550">
+          <div class="carousel-caption d-none d-md-block mb-4">
+            <H3><strong>MONITORAMENTO</strong></H3>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="../../../img/institucional/rastreamento.png" class="d-block w-100 imgcarousel" alt="..." height="550">
+          <div class="carousel-caption d-none d-md-block mb-4">
+            <H3><strong>RASTREAMENTO</strong></H3>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="../../../img/institucional/cnh.jpg" class="d-block w-100 imgcarousel" alt="..." height="550">
+          <div class="carousel-caption d-none d-md-block mb-4">
+            <H3><strong>CNH E MOPP</strong></H3>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
+    <br><br>
+    <div class="container-fluid">
+
+
+     <center>
+      <h1>CONECTANDO O VEÍCULO AO SEU NEGÓCIO</h1>
+      <p>VOCÊ NÃO PRECISA TER UM GERENTE DE FROTAS PARA UTILIZAR A ROTA EXATA. SIMPLES DE USAR, ALTA TECNOLOGIA PARA FROTAS<br> PEQUENAS ATRAVÉS DE UM CONJUNTO COMPLETO DE PRODUTOS PARA RASTREAMENTO VEICULAR, GESTÃO DE FROTAS, ROTEIRIZAÇÃO E <br>GERENCIAMENTO DE ENTREGAS E SERVIÇOS: <strong>TUDO INTEGRADO EM UMA ÚNICA PLATAFORMA.</strong></p>
+    </center>
+
+    <br><br><br>
+
+
+    <div class="row">
+      <div class="card col m-3 text-center sombra" style="width: 18rem;">
+        <div class="col mt-4"><img class="imgcard" src="../../../img/institucional/monitoramento1.png" alt="..."></div>
+        <div class="card-body m-3">
+          <h3 class="card-title">MONITORAMENTO</h3>  
+          <center>
+              <!-- Scroll to Top Button-->
+              <a class="rounded" href="#page-monitoramento">
+                  <i class="fas fa-angle-down"></i>
+              </a>
+          </center>
+
+        </div>
+      </div>
+
+      <div class="card col m-3 text-center sombra" style="width: 18rem;">
+        <div class="col mt-4"><img class="imgcard" src="../../../img/institucional/rastreamento1.jpg" alt="..."></div>
+        <div class="card-body m-3">
+          <h3 class="card-title">RASTREAMENTO</h3>  
+          <center>
+            <a class="list-group-item list-group-item-action btnscrol col-4 mt-4" href="#list-item-2"><img height="30" width="30" src="../../../img/institucional/inicial_icons/down.gif"></a>
+          </center>
+        </div>
+      </div>
+
+      <div class="card col m-3 text-center sombra" style="width: 18rem;">
+        <div class="col mt-4"><img class="imgcard" src="../../../img/institucional/cnh1.jpg" alt="..."></div>
+        <div class="card-body m-3">
+          <h3 class="card-title">CNH e MOPP</h3>  
+          <center>
+            <a class="list-group-item list-group-item-action btnscrol col-4 mt-4 btn-white" href="#list-item-3"><img height="30" width="30" src="../../../img/institucional/inicial_icons/down.gif"></a>
+          </center>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<br><br><br><br><br id="list-item-2"><br><br><br>
+
+<center>
+  <div data-spy="scroll" data-target="#list-example" data-offset="0" class="scrollspy-example">
+      
+      <div class="col-12">
+        <h4 class="text-center mb-4">RASTREAMENTO</h4>
+        <div class="mb-5">
+          <img class="sombra" style="border-radius: 15px;" src="../../../img/institucional/rastreagif.gif" width="600" height="350">
+        </div>
+        <h5 class="mb-5 text-center"><strong>Rastreie suas frotas para uma melhor precisão em suas entregas.</strong></h5>
+        <a class="btn btn-primary" href="../../../index.php" role="button">EXPERIMENTE AGORA</a>
+      </div>
+
+      <br><br><br id="list-item-1"><br><br><br>
+
+      <div class="col-12" id="page-monitoramento">
+        <h4 class="text-center mb-4">MONITORAMENTO</h4>
+        <div class="mb-5">
+          <img class="sombra" style="border-radius: 15px;" src="../../../img/institucional/monitorgif.gif" width="600" height="350">
+        </div>
+        <h5 class="mb-5 text-center"><strong>Monitore seus motoristas, tenha controle e relatórios de horários, diárias entre outros.</strong></h5>
+        <a class="btn btn-primary" href="../../../index.php" role="button">EXPERIMENTE AGORA</a>
+      </div>
+
+      <br><br><br id="list-item-3"><br><br><br>
+
+      <div class="col-12">
+        <h4 class="text-center mb-4">CNH E MOPP</h4>
+        <div class="mb-5">
+          <img class="sombra" style="border-radius: 15px;" src="../../../img/institucional/cnhgif.gif" width="600" height="350">
+        </div>        
+        <h5 class="mb-5 text-center"><strong>Garanta a segurança em sua empresa.
+        Avisos sobre vencimento de CNH e MOPP serão enviados aos motoristas.</strong></h5>
+        <a class="btn btn-primary" href="../../../index.php" role="button">EXPERIMENTE AGORA</a>
+      </div>
+  </div>
+
+  <br id="list-item-4"><br><br><br><br>
+
+  <div class="container-fluid pt-5 divfotos bg-indigo">
+    <h2 class="card-title text-center text-white mb-5">QUEM SOMOS ?</h2>
+
+    <div class="row pt-3">
+
+      <div class="card col border-0 divperfil">
+        <img class="rounded-circle mt-2 col foto" src="../../../img/institucional/perfil/douglas.png" alt="Imagem de capa do card">
+        <div class="card-body text-center">
+         <h4 class="card-title text-white">Douglas Ronchi</h4>
+         <a href="https://www.linkedin.com/in/douglas-ronchi-7b5a2b134?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BIB6XpEfMSHqmMjokcZRiCw%3D%3D" target="_blank">
+           <img class="btn btn-light" src="../../../img/institucional/inicial_icons/in.png" height="33" width="100">
+         </a>
+       </div>
+     </div>
+
+     <div class="card col border-0 divperfil">
+      <img class="rounded-circle mt-2 col foto" src="../../../img/institucional/perfil/paloma.jpg" alt="Imagem de capa do card">
+      <div class="card-body text-center">
+        <h4 class="card-title text-white">Paloma Teply</h4>
+        <a href="#" target="_blank">
+          <img class="btn btn-light" src="../../../img/institucional/inicial_icons/in.png" height="33" width="100">
+        </a>
+      </div>
+    </div>
+
+    <div class="card col border-0 divperfil">
+      <img class="rounded-circle mt-2 col foto" src="../../../img/institucional/perfil/giovane.png" alt="Imagem de capa do card">
+      <div class="card-body text-center">
+        <h4 class="card-title text-white">Geovane Duarte</h4>
+        <a href="https://www.linkedin.com/in/giovane-duarte-bb6786132?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BYLCcM9djRlK8lMJAcgWG8Q%3D%3D" target="_blank">
+          <img class="btn btn-light" src="../../../img/institucional/inicial_icons/in.png" height="33" width="100">
+        </a>
+      </div>
+    </div>
+
+    <div class="card col border-0 divperfil">
+      <img class="rounded-circle mt-2 col foto" src="../../../img/institucional/perfil/dauana.jpg" alt="Imagem de capa do card">
+      <div class="card-body text-center">
+        <h4 class="card-title text-white">Dauana Severo</h4>
+        <a href="https://www.linkedin.com/in/dauana-severo-bb152a18a?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BaWjt6wBpRle6nhDbEtZ0nQ%3D%3D" target="_blank">
+          <img class="btn btn-light" src="../../../img/institucional/inicial_icons/in.png" height="33" width="100">
+        </a>
+      </div>
+    </div>
+
+    <div class="card col border-0 divperfil">
+      <img class="rounded-circle mt-2 col foto" src="../../../img/institucional/perfil/jonas.png" alt="Imagem de capa do card">
+      <div class="card-body text-center">
+        <h4 class="card-title text-white">Jonas Antunes</h4>
+        <a href="https://www.linkedin.com/in/jonas-antunes-049197187?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B7FBba2MVRt6sC%2F4fQ26CUA%3D%3D" target="_blank">
+          <img class="btn btn-light" src="../../../img/institucional/inicial_icons/in.png" height="33" width="100">
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+</center>
+
+<br><br><br><br><br>
+
+<div id="list-item-5" class="container-fluid contatos">
+  <div class="row">
+
+    <a href="https://web.facebook.com/Pojigo-101839457873456" target="_blank" class="card col text-white bg-transparent text-center cardcontatos">
+      <div class="card-body col">
+        <img src="../../../img/institucional/inicial_icons/face.png" height="60" width="70">
+        <h5 class="mt-1">Pagina facebook</h5>
+      </div>
+    </a>
+    
+    <div class="card col text-white bg-transparent text-center cardcontatos">
+      <div class="card-body col">
+        <img src="../../../img/institucional/inicial_icons/contato.png" height="60" width="70">
+        <h5 class="mt-2">(47) 33546588</h5>
+      </div>
+    </div>
+
+    <div class="card col text-white bg-transparent text-center cardcontatos">
+      <div class="card-body col">
+        <img src="../../../img/institucional/inicial_icons/whats.png" height="70" width="130">
+        <h5 class="">(47) 996545424</h5>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+      <i class="fas fa-angle-up"></i>
+  </a>
+
+<footer class="sombra text-center">
+  <div class="p-4 text-dark">
+    <strong>Copyright © Your Website 2019</strong>
+  </div>
+</footer>
+
+
+<script type="text/javascript">
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    mybutton.style.display = "block";
+    $('.navbar').css("background-color", "rgba(0, 0, 0, 0.8)");
+  } else {
+    mybutton.style.display = "none";
+    $('.navbar').css("background-color", "transparent");
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+</script>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="../../../vendor/jquery/jquery.min.js"></script>
+  <script src="../../../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="../../../vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="../../../js/sb-admin-2.min.js"></script>
+
+  <script src="../../../js/funcoesMobile.js"></script>
+</body>
+
+</html>
