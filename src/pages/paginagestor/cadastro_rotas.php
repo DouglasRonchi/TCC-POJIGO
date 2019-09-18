042283103125a670e6439b386ff5921315f9c6ad
Diego Peres <speres.diego@gmail.com>
2019-09-18 09:56:27 -0300

    Adicionando cadastro de rotas

--- src/pages/paginagestor/cadastro_rotas.php
+++ src/pages/paginagestor/cadastro_rotas.php
@@ -126,13 +126,100 @@ $login->VerificarLogin();
           <!-- Page Heading -->
           <h1 class="h3 mb-4 text-gray-800">Cadastro de Rotas</h1>
 
-          <!-- chave google api AIzaSyCbqJXX7fEFddatn-vaBp3BtBS-4TJNIbg -->
+          <!-- Parâmetro sensor é utilizado somente em dispositivos com GPS -->
+    <script src="http://maps.google.com/maps/api/js?sensor=false"></script>
+    <script type="text/javascript">
+        function CalculaDistancia() {
+            $('#litResultado').html('Aguarde...');
+            //Instanciar o DistanceMatrixService
+            var service = new google.maps.DistanceMatrixService();
+            //executar o DistanceMatrixService
+            service.getDistanceMatrix(
+              {
+                  //Origem
+                  origins: [$("#txtOrigem").val()],
+                  //Destino
+                  destinations: [$("#txtDestino").val()],
+                  //Modo (DRIVING | WALKING | BICYCLING)
+                  travelMode: google.maps.TravelMode.DRIVING,
+                  //Sistema de medida (METRIC | IMPERIAL)
+                  unitSystem: google.maps.UnitSystem.METRIC
+                  //Vai chamar o callback
+              }, callback);
+        }
+        //Tratar o retorno do DistanceMatrixService
+        function callback(response, status) {
+            //Verificar o Status
+            if (status != google.maps.DistanceMatrixStatus.OK)
+                //Se o status não for "OK"
+                $('#litResultado').html(status);
+            else {
+                //Se o status for OK
+                //Endereço de origem = response.originAddresses
+                //Endereço de destino = response.destinationAddresses
+                //Distância = response.rows[0].elements[0].distance.text
+                //Duração = response.rows[0].elements[0].duration.text
+                $('#litResultado').html("<strong>Origem</strong>: " + response.originAddresses +
+                    "<br /><strong>Destino:</strong> " + response.destinationAddresses +
+                    "<br /><strong>Distância</strong>: " + response.rows[0].elements[0].distance.text +
+                    " <br /><strong>Duração</strong>: " + response.rows[0].elements[0].duration.text
+                    );
+                //Atualizar o mapa
+                $("#map").attr("src", "https://maps.google.com/maps?saddr=" + response.originAddresses + "&daddr=" + response.destinationAddresses + "&output=embed");
+            }
+        }
+    </script>
+    <form action="../../controllers/rotasController.php" method="post">
+    <table width="100%" cellspacing="0" cellpadding="0" border="0">
+        <tbody>
+            <tr>
+                <td>
+                    <label for="txtOrigem"><strong>Endere&ccedil;o de origem</strong></label>
+                    <input type="text" id="txtOrigem" class="field" name="origem" style="width: 400px" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="txtDestino"><strong>Endere&ccedil;o de destino</strong></label>
+                    <input type="text" style="width: 400px" class="field" id="txtDestino" name="destino" />
 
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="button" value="Calcular dist&acirc;ncia" onclick="CalculaDistancia()" class="btnNew" />
+                </td>
+                <td>
+                    <input type="submit" value="Salvar Rotas" class="btnNew" name="salvar"" />
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    </form>
+    <div><span id="litResultado">&nbsp;</span></div>
+    <div style="padding: 10px 0 0; clear: both">
+        <iframe width="750" scrolling="no" height="350" frameborder="0" id="map" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?saddr=são paulo&daddr=rio de janeiro&output=embed"></iframe>
+      </div>
+      <script>
 
+        function inicializar() {
+          var coordenadas = {lat: -22.912869, lng: -43.228963};
 
+          var mapa = new google.maps.Map(document.getElementById('mapa'), {
+            zoom: 15,
+            center: coordenadas
+          });
 
-
-
+          var marker = new google.maps.Marker({
+            position: coordenadas,
+            map: mapa,
+            title: 'Meu marcador'
+          });
+        }
+      </script>
+      <script async defer
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCbqJXX7fEFddatn-vaBp3BtBS-4TJNIbg&callback=inicializar">
+    </script>
         </div>
   <!-- /.container-fluid -->
 
