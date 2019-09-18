ccf3662c7704a3d40545e51edf3c8f4a9ea532e8
Diego Peres <speres.diego@gmail.com>
2019-09-18 09:56:27 -0300

    Adicionando cadastro de rotas

---
+++ src/controllers/rotasController.php
@@ -0,0 +1,13 @@
+<?php
+    require_once '../classes/Autoload.class.php';
+    $conn = New Site;
+
+    $origem = $_POST['origem'];
+    $destino = $_POST['destino'];
+
+    if (isset($_POST['salvar'])) {
+        
+    }
+
+    echo "fffuck";
+?>
