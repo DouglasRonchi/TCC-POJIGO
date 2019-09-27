842a5ecbf35f6cc25a144b295cafe7a7db1b1f5e
DouglasRonchi <douglasronchi02@hotmail.com>
2019-09-27 09:32:46 -0300

    Ajustando Rastreamento e Dashboard

--- src/pages/menu/topbar.php
+++ src/pages/menu/topbar.php
@@ -1,7 +1,7 @@
 <?php
 $conn = new Site;
 ?>
-
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- Topbar -->
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
@@ -11,18 +11,18 @@ $conn = new Site;
     </button>
 
     <!-- Topbar Search -->
-    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-        <div class="input-group">
-            <input type="text" class="form-control bg-light border-0 small" placeholder="Pesquisar..."
-                   aria-label="Search" aria-describedby="basic-addon2">
-            <div class="input-group-append">
-                <button class="btn btn-primary" type="button">
-                    <i class="fas fa-search fa-sm"></i>
-                </button>
-            </div>
-        </div>
-    </form>
+    <div class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+        <script async src="https://cse.google.com/cse.js?cx=011766867709222014939:rtfjrxmg5cp"></script>
+        <div class="gcse-search"></div>
+    </div>
 
+    <script>
+        window.onload = demo;
+
+        function demo() {
+            document.getElementById("gsc-i-id1").setAttribute("placeholder", "Pesquisar no site...")
+        }
+    </script>
 
     <ul class="navbar-nav ml-auto">
 
@@ -74,7 +74,7 @@ $conn = new Site;
                 while ($result = mysqli_fetch_assoc($query)):
                     if ($result['lida'] == 0):
                         ?>
-                        <div class="d-flex align-items-center border-left-<?= ($result['lida']==1)?'success':'danger';?>">
+                        <div class="d-flex align-items-center border-left-<?= ($result['lida'] == 1) ? 'success' : 'danger'; ?>">
                             <form action="../../controllers/notificacoesController.php" method="post">
                                 <button type="submit" class="dropdown-item d-flex align-items-center btn"
                                         name="clicknotificacao">
@@ -98,7 +98,7 @@ $conn = new Site;
                 ?>
                 <?php
                 if ($qtdNotificacoes == 0):
-                ?>
+                    ?>
                     <a class="dropdown-item d-flex align-items-center" href="#">
                         <div class="mr-3">
                             <div class="icon-circle bg-dark">
@@ -125,63 +125,63 @@ $conn = new Site;
         </li>
 
         <!-- Nav Item - Messages -->
-        <li class="nav-item dropdown no-arrow mx-1">
-            <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown"
-               aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-envelope fa-fw"></i>
-                <!-- Counter - Messages -->
-                <span class="badge badge-danger badge-counter">7</span>
-            </a>
-            <!-- Dropdown - Messages -->
-            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                 aria-labelledby="messagesDropdown">
-                <h6 class="dropdown-header">
-                    Mensagens
-                </h6>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                    <div class="dropdown-list-image mr-3">
-                        <img class="rounded-circle" src="https://source.unsplash.com/fn_BT9fwg_E/60x60" alt="">
-                        <div class="status-indicator bg-success"></div>
-                    </div>
-                    <div class="font-weight-bold">
-                        <div class="text-truncate">Olá! Gostaria de saber se você pode me ajudar com um problema que tenho tido.</div>
-                        <div class="small text-gray-500">Emily Fowler · 58m</div>
-                    </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                    <div class="dropdown-list-image mr-3">
-                        <img class="rounded-circle" src="https://source.unsplash.com/AU4VPcFN4LE/60x60" alt="">
-                        <div class="status-indicator"></div>
-                    </div>
-                    <div>
-                        <div class="text-truncate">Tenho as fotos que você pediu no mês passado, como gostaria que elas fossem enviadas para você?</div>
-                        <div class="small text-gray-500">Jae Chun · 1d</div>
-                    </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                    <div class="dropdown-list-image mr-3">
-                        <img class="rounded-circle" src="https://source.unsplash.com/CS2uCrpNzJY/60x60" alt="">
-                        <div class="status-indicator bg-warning"></div>
-                    </div>
-                    <div>
-                        <div class="text-truncate">O relatório do mês passado está ótimo, estou muito feliz com o progresso até agora, continue com o bom trabalho!</div>
-                        <div class="small text-gray-500">Morgan Alvarez · 2d</div>
-                    </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                    <div class="dropdown-list-image mr-3">
-                        <img class="rounded-circle" src="https://source.unsplash.com/Mv9hjnEUHR4/60x60" alt="">
-                        <div class="status-indicator bg-success"></div>
-                    </div>
-                    <div>
-                        <div class="text-truncate">De os parabéns a equipe em meu nome, estou muito feliz com os resultados apresentados.
-                        </div>
-                        <div class="small text-gray-500">Steve Jobs · 2sem</div>
-                    </div>
-                </a>
-                <a class="dropdown-item text-center small text-gray-500" href="#">Leia mais mensagens</a>
-            </div>
-        </li>
+        <!--        <li class="nav-item dropdown no-arrow mx-1">-->
+        <!--            <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown"-->
+        <!--               aria-haspopup="true" aria-expanded="false">-->
+        <!--                <i class="fas fa-envelope fa-fw"></i>-->
+        <!--                 Counter - Messages -->
+        <!--                <span class="badge badge-danger badge-counter">7</span>-->
+        <!--            </a>-->
+        <!--             Dropdown - Messages -->
+        <!--            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"-->
+        <!--                 aria-labelledby="messagesDropdown">-->
+        <!--                <h6 class="dropdown-header">-->
+        <!--                    Mensagens-->
+        <!--                </h6>-->
+        <!--                <a class="dropdown-item d-flex align-items-center" href="#">-->
+        <!--                    <div class="dropdown-list-image mr-3">-->
+        <!--                        <img class="rounded-circle" src="https://source.unsplash.com/fn_BT9fwg_E/60x60" alt="">-->
+        <!--                        <div class="status-indicator bg-success"></div>-->
+        <!--                    </div>-->
+        <!--                    <div class="font-weight-bold">-->
+        <!--                        <div class="text-truncate">Olá! Gostaria de saber se você pode me ajudar com um problema que tenho tido.</div>-->
+        <!--                        <div class="small text-gray-500">Emily Fowler · 58m</div>-->
+        <!--                    </div>-->
+        <!--                </a>-->
+        <!--                <a class="dropdown-item d-flex align-items-center" href="#">-->
+        <!--                    <div class="dropdown-list-image mr-3">-->
+        <!--                        <img class="rounded-circle" src="https://source.unsplash.com/AU4VPcFN4LE/60x60" alt="">-->
+        <!--                        <div class="status-indicator"></div>-->
+        <!--                    </div>-->
+        <!--                    <div>-->
+        <!--                        <div class="text-truncate">Tenho as fotos que você pediu no mês passado, como gostaria que elas fossem enviadas para você?</div>-->
+        <!--                        <div class="small text-gray-500">Jae Chun · 1d</div>-->
+        <!--                    </div>-->
+        <!--                </a>-->
+        <!--                <a class="dropdown-item d-flex align-items-center" href="#">-->
+        <!--                    <div class="dropdown-list-image mr-3">-->
+        <!--                        <img class="rounded-circle" src="https://source.unsplash.com/CS2uCrpNzJY/60x60" alt="">-->
+        <!--                        <div class="status-indicator bg-warning"></div>-->
+        <!--                    </div>-->
+        <!--                    <div>-->
+        <!--                        <div class="text-truncate">O relatório do mês passado está ótimo, estou muito feliz com o progresso até agora, continue com o bom trabalho!</div>-->
+        <!--                        <div class="small text-gray-500">Morgan Alvarez · 2d</div>-->
+        <!--                    </div>-->
+        <!--                </a>-->
+        <!--                <a class="dropdown-item d-flex align-items-center" href="#">-->
+        <!--                    <div class="dropdown-list-image mr-3">-->
+        <!--                        <img class="rounded-circle" src="https://source.unsplash.com/Mv9hjnEUHR4/60x60" alt="">-->
+        <!--                        <div class="status-indicator bg-success"></div>-->
+        <!--                    </div>-->
+        <!--                    <div>-->
+        <!--                        <div class="text-truncate">De os parabéns a equipe em meu nome, estou muito feliz com os resultados apresentados.-->
+        <!--                        </div>-->
+        <!--                        <div class="small text-gray-500">Steve Jobs · 2sem</div>-->
+        <!--                    </div>-->
+        <!--                </a>-->
+        <!--                <a class="dropdown-item text-center small text-gray-500" href="#">Leia mais mensagens</a>-->
+        <!--            </div>-->
+        <!--        </li>-->
 
         <div class="topbar-divider d-none d-sm-block"></div>
 
@@ -190,23 +190,24 @@ $conn = new Site;
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
                aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= (isset($_SESSION['nome'])) ? utf8_encode($_SESSION['nome']) : ''; ?></span>
-                <img class="img-profile rounded-circle" src="../../../uploads/<?= (isset($_SESSION['foto'])&&$_SESSION['foto']!='') ? $_SESSION['foto'] : 'default.png'; ?>">
+                <img class="img-profile rounded-circle"
+                     src="../../../uploads/<?= (isset($_SESSION['foto']) && $_SESSION['foto'] != '') ? $_SESSION['foto'] : 'default.png'; ?>">
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
-                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Perfil
-                </a>
-                <a class="dropdown-item" href="#">
-                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Configurações
-                </a>
-                <a class="dropdown-item" href="#">
-                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Atividades
-                </a>
-                <div class="dropdown-divider"></div>
+                <!--                <a class="dropdown-item" href="#">-->
+                <!--                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>-->
+                <!--                    Perfil-->
+                <!--                </a>-->
+                <!--                <a class="dropdown-item" href="#">-->
+                <!--                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>-->
+                <!--                    Configurações-->
+                <!--                </a>-->
+                <!--                <a class="dropdown-item" href="#">-->
+                <!--                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>-->
+                <!--                    Atividades-->
+                <!--                </a>-->
+                <!--                <div class="dropdown-divider"></div>-->
                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                     Sair
@@ -234,7 +235,7 @@ $conn = new Site;
                 $result = mysqli_num_rows($query);
                 while ($result = mysqli_fetch_assoc($query)):
                     ?>
-                    <div class="d-block align-items-center border-left-<?= ($result['lida']==1)?'success':'danger';?>">
+                    <div class="d-block align-items-center border-left-<?= ($result['lida'] == 1) ? 'success' : 'danger'; ?>">
                         <form action="../../controllers/notificacoesController.php" method="post">
                             <button type="submit" class="dropdown-item d-flex align-items-center"
                                     name="clicknotificacao">
